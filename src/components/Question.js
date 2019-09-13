bae22ac25b7619b3477e93b3f40a451a369a90e1
Katelyn <katelyncmiles@gmail.com>
2019-09-13 10:57:41 -0400

    can select one radio button for each question

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,6 +6,17 @@ import axios from 'axios'
 const Question = () => {
 
 const [data, setData] = useState({quizzes: []})
+const [isFilled, setFilled] = useState(false)
+const [selectedOption, setSelectedOption] = useState('')
+const [count, setCount] = useState(0)
+const [checked, setChecked] = useState('')
+let circleColor = 'blank'
+// const[count, setCount] = useState(0)
+
+const toggleFilled = () => {
+    setFilled(!isFilled)
+    isFilled ?  circleColor = 'black' : circleColor = 'blank'
+}
 
 
   useEffect (() => {
@@ -18,27 +29,62 @@ const [data, setData] = useState({quizzes: []})
     fetchData();
   }, []);
 
+// handleOptionChange = event => {
+// [event.currentTarget.id]:event.target.value
+// }
+
+const handleOptionChange = event => {
+    setSelectedOption(event.target.value)
+    console.log({selectedOption})
+    }
+
+const handleFormSubmit = formSubmitEvent => {
+    formSubmitEvent.preventDefault();
+    console.log({selectedOption})
+    // selectedOption ? setCount(count + 1) : null
+    // console.log(`${formSubmitEvent.currentTarget.id} is ${selectedOption}`)
+    // console.log(count)
+}
 
 return (
     <div className = "question">
         <div>
-          { data.questions &&
-            data.questions.map(question => {
+        <form className="answer">
+          { 
+              data.questions &&
+              
+            data.questions.map( (question, qindex) => {
               return(
-                <div className="questionBox">
+                <div onClick={handleOptionChange} id={`question${qindex}`} className="questionBox">
+                    <h2>Question {qindex + 1}</h2>
                     <img className ="questionImg" src ={question.img_url}/>
                     <h2>{question.instructions}</h2>
-                    {question.answers.map(answer => {
+                    {question.answers.map( (answer, index )=> {
                         return (
-                            <div className="answer">
-                                <div className="circle"></div><h3 className="option">{answer.content}</h3>
-                            </div>
+                            <label>
+                                <input 
+                                index={index} 
+                                type="radio" 
+                                name={`option${qindex}`} 
+                                // checked={setChecked(`option${index}`)}
+                                value={`option${index}`}
+                                /> 
+                                {answer.content}
+                            </label>
                         )
-                    })}
+                    })
+                    }
+            
                     {/* <Answer/> */}
                 </div>
               )
-          })}
+          })
+          
+          }
+            <br/>
+            <input type="submit" onSubmit={() => handleFormSubmit()}/>
+        </form>
+          
           
         </div>
         {/* {
@@ -50,9 +96,11 @@ return (
         } */}
         
         <br/>
-        <button>Submit Answers</button>
     </div>
 )
 }
 
 export default Question
+
+
+{/* <div onClick={()=>toggleFilled()} className={circleColor}>.</div> */}
