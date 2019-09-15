be96d3cef2619a2532e904ab77e593d4c693055d
Katelyn <katelyncmiles@gmail.com>
2019-09-15 15:46:56 -0400

    added react materialize and responsive side nav button but radio buttons went away

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
-import Quiz from './components/Quiz'
+import { SideNav, SideNavItem, Button } from 'react-materialize';
+import Navigation from 'react-toolbox/lib/navigation';
+// import Link from 'react-toolbox/lib/link';
+// import Quiz from './components/Quiz'
 import Question from './components/Question'
-import Slider from "react-slick";
 import './App.css';
 import axios from 'axios'
 
@@ -26,20 +28,24 @@ const App = () => {
     <div className="App">
         <Router>
         <header>
-        <Link to="/" className="siteTitle">Music Theory Quizzes</Link>
+          <Link to="/" className="siteTitle">Music Theory Quizzes</Link>
+        </header>
           <div className = "container">
-            <nav>
-              <Link to="/quiz/1" className = "quizLink">Basic Intervals</Link>
-              <Link to="/quiz/2" className = "quizLink">Triad Chords</Link>
-              <Link to="/quiz/3" className = "quizLink">Quiz 3</Link>
-            </nav>
+            <div className ='navButton'>
+            <SideNav trigger={<Button waves="light">Quizzes</Button>} options={{closeOnClick: true}}>
+              <SideNavItem href="#!icon"><Link to="/quiz/1" className = "quizLink" label='Basic Intervals'>Basic Intervals</Link></SideNavItem>
+              <SideNavItem href="#!second"><Link to="/quiz/2" className = "quizLink" label='Triad Chords'>Triad Chords</Link></SideNavItem>
+              <SideNavItem waves href="#!third"><Link to="/quiz/3" className = "quizLink">Quiz 3</Link></SideNavItem>
+            </SideNav>
+            
+            </div>
             {/* <Route path="/Quiz1" component={Quiz} /> */}
             <Switch>
               <Route path="/quiz/:id" component={Question} />
               {/* <Route path="/count" component={Answer}/> */}
             </Switch>
           </div>
-      </header>
+      
         {/* <Link to='/count'>Submit Answers and Get Score!</Link> */}
       </Router>
       {/* {!isShowing? <h2 onClick={()=>toggleQuiz()} className="quiz1">Basic Intervals</h2> : null} */}
@@ -58,7 +64,10 @@ const App = () => {
 
     
     </div>
+    
   );
+  
 }
+
 
 export default App;
